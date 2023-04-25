2fa3d6282fc2617f11f638ddce74de8e4a974947
Elon Abergel <elonabergel@Elons-MBP.wireless.yale.internal>
2023-04-24 20:41:33 -0400

    user rise hand and go left or right

--- page3.js
+++ page3.js
@@ -15,9 +15,8 @@ var frames = {
 		var url = 'ws://' + host + '/frames';
 		frames.socket = new WebSocket(url);
 		frames.socket.onmessage = function (event) {
-			var command = frames.data_update(JSON.parse(event.data));
+			var command = frames.mouse(JSON.parse(event.data));
 		};
-		this.mouse();
 	},
 
 	// In this function we need to follow the mouse so the user can "click on each of the images."
@@ -25,55 +24,51 @@ var frames = {
 		if (frame.people.length < 1) {
 			return null;
 		}
+		user_x_value = null;
+		positions = frame.people[0];
+
 		// We need to follow the curser to see if it is on one of the images!
 		var opt1 = document.querySelector('img[src="pics/opt1.png"]');
 		var opt3 = document.querySelector('img[src="pics/opt3.png"]');
 		var cursor = document.querySelector('.cursor');
+		var HAND_RIGHT_X = positions.joints[14].position.x;
+		var HAND_RIGHT_Y = positions.joints[14].position.y;
 
-		// Set up event listeners for mouseover and mouseout events
-		opt1.addEventListener('mouseover', function () {
-			cursor.classList.add('pointer');
-			setTimeout(function () {
+		var pelvis_y = positions.joints[2].position.y;
+
+		var left_elbow_y = positions.joints[6].position.y;
+		var chest_y = positions.joints[2].position.y;
+		var left_wrist_y = positions.joints[7].position.y;
+		var nose_y = positions.joints[27].position.y;
+		var right_elbow_y = positions.joints[13].position.y;
+		var right_wrist_y = positions.joints[14].position.y;
+
+		if (right_elbow_y < nose_y && right_wrist_y < nose_y) {
+			if (HAND_RIGHT_X > 500) {
+				// if (HAND_RIGHT_Y > )
 				window.location.href = 'page5.html';
-			}, 2000); // Redirect after 2 seconds
-		});
-
-		opt1.addEventListener('mouseout', function () {
-			cursor.classList.remove('pointer');
-		});
-
-		opt3.addEventListener('mouseover', function () {
-			cursor.classList.add('pointer');
-			setTimeout(function () {
+			} else if (HAND_RIGHT_X < -70) {
 				window.location.href = 'page5_lower.html';
-			}, 2000); // Redirect after 2 seconds
-		});
-
-		opt3.addEventListener('mouseout', function () {
-			cursor.classList.remove('pointer');
-		});
-
-		// var HAND_LEFT_X = positions.joints[8].position.x;
-		// var HAND_LEFT_Y = positions.joints[8].position.y;
-		var HAND_RIGHT_X = positions.joints[15].position.x;
-		var HAND_RIGHT_Y = positions.joints[15].position.y;
-
-		function updateCursor(x, y) {
-			cursor.style.top = y + 'px';
-			cursor.style.left = x + 'px';
+			}
 		}
-		updateCursor(HAND_RIGHT_X, HAND_RIGHT_Y);
+		// function updateCursor(x) {
+		// 	cursor.style.left = HAND_RIGHT_X + 'px';
+		// }
+		// // the user moved his hand to the left
+		// if (HAND_RIGHT_X > 0) {
+		// 	updateCursor(5);
+
+		// 	// user_x_value = HAND_RIGHT_X;
+		// }
+		// // the user moved his hand to the right
+		// else if (HAND_RIGHT_X > 0) {
+		// 	updateCursor(-5);
+		// }
 
 		// If the user
 	},
 
 	// In this function we need to pass information to the cursoer function so we can update it every time.
-	data_update: function (frame) {
-		if (frame.people.length < 1) {
-			return null;
-		}
-		positions = frame.people[0];
-	},
 };
 
 var twod = {
